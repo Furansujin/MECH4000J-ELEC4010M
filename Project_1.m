6e633fe0afb7861de177b0b2c6543d8dbc389b60
trns1997 <trns@ust.hk>
2019-03-20 20:09:52 +0800

    TS_initials

--- Project_1.m
+++ Project_1.m
@@ -16,9 +16,9 @@ figure(1);
 pl = inv_kin(807.6, 0, 191.4, p,  10, 'r');
 
 %% Write MA INITIALS BITCH ASS BAXTER
-st = 1;
-x = [(0:st:50), (50:-st:25), 25*ones(1,length(200:-st:150))];
-y = [200*ones(1,length(0:st:50)), 200*ones(1,length(50:-st:25)), (200:-st:150)];
+st = 5;
+x = [(0:st:50), (50:-st:25), 25*ones(1,length(200:-st:150)), (55:st:105), 105*ones(1, length(150:st:175)), (105:-st:55), 55*ones(1, length(175:st:200)), (55:st:105)];
+y = [200*ones(1,length(0:st:50)), 200*ones(1,length(50:-st:25)), (200:-st:150), 150*ones(1, length(55:st:105)), (150:st:175), 175*ones(1, length(105:-st:55)), (175:st:200), 200*ones(1, length(55:st:105))];
 z = zeros(1, length(x));
 T = horzcat(x', y', z');
 for i = 1:length(T(:,1))
--- p_cloud.m
+++ p_cloud.m
@@ -19,7 +19,9 @@ q = T6;
 xM = 69.*cos(l) - 10.*cos(p).*(cos(o).*(sin(l).*sin(n) + cos(l).*cos(n).*sin(m)) + cos(l).*cos(m).*sin(o)) - (3683.*cos(q).*(sin(o).*(sin(l).*sin(n) + cos(l).*cos(n).*sin(m)) - cos(l).*cos(m).*cos(o)))/10 - 69.*sin(l).*sin(n) - (3683.*sin(q).*(cos(p).*(cos(o).*(sin(l).*sin(n) + cos(l).*cos(n).*sin(m)) + cos(l).*cos(m).*sin(o)) + sin(p).*(cos(n).*sin(l) - cos(l).*sin(m).*sin(n))))/10 - (37429.*sin(o).*(sin(l).*sin(n) + cos(l).*cos(n).*sin(m)))/100 - 10.*sin(p).*(cos(n).*sin(l) - cos(l).*sin(m).*sin(n)) + (7287.*cos(l).*cos(m))/20 + (37429.*cos(l).*cos(m).*cos(o))/100 - 69.*cos(l).*cos(n).*sin(m);
 yM = 69.*sin(l) + (7287.*cos(m).*sin(l))/20 + 69.*cos(l).*sin(n) + 10.*cos(p).*(cos(o).*(cos(l).*sin(n) - cos(n).*sin(l).*sin(m)) - cos(m).*sin(l).*sin(o)) + (3683.*cos(q).*(sin(o).*(cos(l).*sin(n) - cos(n).*sin(l).*sin(m)) + cos(m).*cos(o).*sin(l)))/10 + (3683.*sin(q).*(cos(p).*(cos(o).*(cos(l).*sin(n) - cos(n).*sin(l).*sin(m)) - cos(m).*sin(l).*sin(o)) + sin(p).*(cos(l).*cos(n) + sin(l).*sin(m).*sin(n))))/10 + (37429.*sin(o).*(cos(l).*sin(n) - cos(n).*sin(l).*sin(m)))/100 + 10.*sin(p).*(cos(l).*cos(n) + sin(l).*sin(m).*sin(n)) + (37429.*cos(m).*cos(o).*sin(l))/100 - 69.*cos(n).*sin(l).*sin(m); 
 zM = (3683.*sin(q).*(cos(p).*(sin(m).*sin(o) - cos(m).*cos(n).*cos(o)) + cos(m).*sin(n).*sin(p)))/10 - 69.*cos(m).*cos(n) - (37429.*cos(o).*sin(m))/100 - (7287.*sin(m))/20 + 10.*cos(p).*(sin(m).*sin(o) - cos(m).*cos(n).*cos(o)) - (3683.*cos(q).*(cos(o).*sin(m) + cos(m).*cos(n).*sin(o)))/10 - (37429.*cos(m).*cos(n).*sin(o))/100 + 10.*cos(m).*sin(n).*sin(p) + 5407/20; 
-plot3(xM(:),yM(:),zM(:),'k.')
+% p = zM(:) > 0;
+% plot3(xM(:).*p,yM(:).*p,zM(:).*p,'k.')
+plot3(xM(:),yM(:),zM(:),'k.');
 axis equal
 points = [xM(:),yM(:),zM(:)];
 
