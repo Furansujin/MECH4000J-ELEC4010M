07ca210870639aadb798e90250807b0c6dc66c27
trns1997 <trns@ust.hk>
2019-03-29 10:46:22 +0800

    comments

--- Project_1.m
+++ Project_1.m
@@ -2,6 +2,8 @@
 clc, clear all
 
 %% Forward Kinematics
+% fwd_kin(angle_joint_1, angle_joint_2, angle_joint_3, angle_joint_4, ...
+%                  angle_joint_5, angle_joint_6, angle_joint_7, plot_color)
 figure(1);
 fwd_kin(0,0,0,0,0,0,0, 'r');
 hold on;
@@ -9,12 +11,15 @@ hold on;
 %% Point Cloud
 figure(2);
 tic;
-p = p_cloud(10);
+p = p_cloud(10); % p_cloud(vector_spacing)
 toc;
 %% Inverse Kinematics
+% inv_kin(x_pos, y_pos, z_pos, pre-calculated point cloud or [], ...
+%                                               vector_spacing, plot_color)
 figure(1);
 tic;
 pl = inv_kin(1176, 0, 191.4, p,  10, 'r');
 toc;
 %% Write MA INITIALS BITCH ASS BAXTER
+% wrt_init(point_spacing, pre-calculated point cloud (recommended) or [])
 wrt_init(5, p);
