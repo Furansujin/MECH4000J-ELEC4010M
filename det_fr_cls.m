f3950d87723111bfd5398db7d0f8c8a6e7c6483e
trns1997 <trns@ust.hk>
2019-05-07 12:49:00 +0800

    Project_2

---
+++ det_fr_cls.m
@@ -0,0 +1,30 @@
+function det_fr_cls(v, ln1, ln2, ind1, ind2, u)
+    x1 = [v(ln1,1), v(ln1+1,1)];
+    y1 = [v(ln1,2), v(ln1+1,2)];
+    [n, l, m] = plt_cone(u, x1, y1, ind1, v, 'r');
+    hold on
+    if ln2 == length(v)
+        x1 = [v(ln2,1), v(1,1)];
+        y1 = [v(ln2,2), v(1,2)];
+    else
+        x1 = [v(ln2,1), v(ln2+1,1)];
+        y1 = [v(ln2,2), v(ln2+1,2)];
+    end
+    [n1, l1, m1] = plt_cone(u, x1, y1, ind2, v, 'g');
+
+    plot([l, l1], [m, m1], 'k');
+
+    coef = polyfit([l, l1], [m, m1], 1);
+
+    o = -1/n(1);
+    o1 = -1/n1(1);
+    t = atan((coef(1)-(o))/(1+(o*coef(1))));
+    t1 = atan((coef(1)-(o1))/(1+(o1*coef(1))));
+
+    if abs(rad2deg(t)) < u && abs(rad2deg(t1)) < u
+        fprintf("force closure")
+%         inv_kin(l, m, 0, [],  1, 'b');
+    else
+        fprintf("not force closure")
+    end
+end
