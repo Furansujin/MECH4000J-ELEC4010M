d341c16df09eed8f75610cb70a77dc5eb39ea422
trns1997 <trns@ust.hk>
2019-05-18 12:27:11 +0800

    ppt

--- Project_2/plt_rbt.m
+++ Project_2/plt_rbt.m
@@ -6,4 +6,7 @@ function plt_rbt(v, x0, y0, x1, y1, fc)
         inv_kin(x0 - (x_cen - max(v(:,1))/1.5), y0 - y_cen + 175, 0, [],  1, [(x_cen - max(v(:,1))/1.5), -(-y_cen + 175),0],  'b');
         inv_kin(x1 - (x_cen - max(v(:,1))/1.5), y1 - y_cen - 175, 0, [],  1, [(x_cen - max(v(:,1))/1.5),-(-y_cen - 175),0], 'b');
     end
+    x_cen = mean([min(v(:,1)) max(v(:,1))]);
+    y_cen = mean([min(v(:,2)) max(v(:,2))]);
+    axis([(min(v(:,1)) - x_cen) (max(v(:,1))+ x_cen) (min(v(:,2))- 2*y_cen) (max(v(:,2)+ 2*y_cen))])
 end
